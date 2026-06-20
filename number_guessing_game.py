bfd91e6609b90151fccbaf0c6099fd86bcc3207d
mulelakshmireddy06-hue <mule.lakshmireddy06@gmail.com>
2026-06-20 11:26:46 +0530

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,1 +1,15 @@
+import random 
+random_ = random.randint(1,100)
 
+while True:
+  guess_ = int(input("Guess the Correct number (1 - 100) : "))
+  
+  if(guess_ == random_):
+    print("You Guessed a correct number.")
+    break
+  elif(guess_ < random_):
+    print("You Guessed too low")
+  elif(guess_ > random_):
+    print("You Guessed too high")
+  else :
+    print("Enter Valid number between 1 to 100")
